c3a1a9465efd8f5b85385258d41100a356a48dd0
gabrielQ12 <gabriel.quinette1@gmail.com>
2024-02-09 17:25:20 +0100

    menue:DONE

---
+++ MonRestoAsiatiaue/MonRestoAsiatiaue/Food.swift
@@ -0,0 +1,15 @@
+//
+//  Food.swift
+//  MonRestoAsiatiaue
+//
+//  Created by Gaby on 09/02/2024.
+//
+
+import Foundation
+
+struct Food {
+    var name: String
+    var desc: String
+    var country: String
+    var image: String
+}
